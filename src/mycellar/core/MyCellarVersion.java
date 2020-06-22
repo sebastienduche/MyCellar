1ed0edefd1c3856f71b319c25278a7f2f65f9b58
Sébastien Duché <sebastienduche@hotmail.com>
2020-06-22 07:51:52 +0200

    Fix error reading empty line

--- src/mycellar/core/MyCellarVersion.java
+++ src/mycellar/core/MyCellarVersion.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 public class MyCellarVersion {
 
-	public static final String VERSION = "3.4.6.3";
+	public static final String VERSION = "3.4.6.4";
 	public static final String MAIN_VERSION = Program.VERSION + " NL";
 
 	public static String getLocalVersion() {

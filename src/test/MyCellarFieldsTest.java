3000bb83dc79b701b684d6ef45dbaec4b548ee17
Sébastien Duché <sebastienduche@hotmail.com>
2020-11-20 15:58:53 +0100

    Clean file

--- src/test/MyCellarFieldsTest.java
+++ src/test/MyCellarFieldsTest.java
@@ -5,7 +5,6 @@ import mycellar.core.MyCellarFields;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;

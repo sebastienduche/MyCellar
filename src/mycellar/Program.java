03099a113476ba2f15465c2a230d7418c20fb5f1
Sebastien Duche <Sebastien.Duche@triodos.com>
2021-05-17 13:10:33 +0200

    Fix an issue when replacing a bottle. Tried to clean the wrong cell

--- src/mycellar/Program.java
+++ src/mycellar/Program.java
@@ -119,7 +119,7 @@ import static mycellar.core.MyCellarSettings.PROGRAM_TYPE;
 
 public final class Program {
 
-	public static final String INTERNAL_VERSION = "4.0.2.6";
+	public static final String INTERNAL_VERSION = "4.0.2.9";
 	public static final int VERSION = 68;
 	static final String INFOS_VERSION = " 2021 v";
 	private static Type programType = Type.WINE;
@@ -190,7 +190,15 @@ public final class Program {
 	enum Type {
 		WINE,
 		BOOK,
-		DISC
+		DISC;
+
+		static Type typeOf(String value) {
+			try {
+				return valueOf(value);
+			} catch (IllegalArgumentException e) {
+				return WINE;
+			}
+		}
 	}
 
 	public static void start() throws UnableToOpenFileException {

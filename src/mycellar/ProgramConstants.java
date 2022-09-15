15a776ce9a3d611e256d89692c5c517bc2aeaea4
Sébastien Duché <sebastienduche@hotmail.com>
2022-09-15 22:21:00 +0200

    Update version

--- src/mycellar/ProgramConstants.java
+++ src/mycellar/ProgramConstants.java
@@ -16,9 +16,9 @@ import java.time.format.DateTimeFormatter;
  */
 public final class ProgramConstants {
 
-  public static final String INTERNAL_VERSION = "4.9.4.8";
+  public static final String INTERNAL_VERSION = "4.9.4.9";
   public static final int VERSION = 75;
-  public static final String MAIN_VERSION = VERSION + " BI";
+  public static final String MAIN_VERSION = VERSION + " QE";
   public static final String DEFAULT_STORAGE_EN = "Default storage";
   public static final String DEFAULT_STORAGE_FR = "Rangement par défaut";
 

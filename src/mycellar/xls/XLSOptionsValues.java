5e8090f477be199353af8b0ac4fb26566e2f79be
Sébastien Duché <Sebastien.Duche@triodos.com>
2025-04-04 14:14:47 +0200

    Cleaning

--- src/mycellar/xls/XLSOptionsValues.java
+++ src/mycellar/xls/XLSOptionsValues.java
@@ -1,127 +1,70 @@
 package mycellar.xls;
 
 import javax.swing.table.AbstractTableModel;
+import java.io.Serial;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * <p>Titre : Cave à vin</p>
- * <p>Description : Votre description</p>
- * <p>Copyright : Copyright (c) 2006</p>
- * <p>Société : Seb Informatique</p>
+ * <p>Titre : Cave &agrave; vin
+ * <p>Description : Votre description
+ * <p>Copyright : Copyright (c) 1998
+ * <p>Soci&eacute;t&eacute; : Seb Informatique
  *
- * @author Sébastien Duché
+ * @author S&eacute;bastien Duch&eacute;
  * @version 0.3
  * @since 14/06/20
  */
 class XLSOptionsValues extends AbstractTableModel {
-  public static final int ETAT = 0;
-  static final long serialVersionUID = 260706;
+  static final int ETAT = 0;
+  @Serial
+  private static final long serialVersionUID = -3584372585227509153L;
   private final String[] columnNames = {"", ""};
   private final List<String> oVector = new LinkedList<>();
   private final List<Boolean> oBoolVector = new LinkedList<>();
   private Object[][] values = {{false, ""}};
 
-  /**
-   * getRowCount
-   *
-   * @return int
-   */
   @Override
   public int getRowCount() {
     return values.length;
   }
 
-  /**
-   * getColumnCount
-   *
-   * @return int
-   */
   @Override
   public int getColumnCount() {
     return values[0].length;
   }
 
-  /**
-   * getValueAt
-   *
-   * @param row    int
-   * @param column int
-   * @return Object
-   */
   @Override
   public Object getValueAt(int row, int column) {
     return values[row][column];
   }
 
-  /**
-   * getColumnName
-   *
-   * @param column int
-   * @return String
-   */
   @Override
   public String getColumnName(int column) {
     return columnNames[column];
   }
 
-  /**
-   * getColumnClass
-   *
-   * @param column int
-   * @return Class
-   */
-  @Override
-  public Class<?> getColumnClass(int column) {
-    Class<?> dataType = super.getColumnClass(column);
-
-    return dataType;
-  }
-
-  /**
-   * isCellEditable
-   *
-   * @param row    int
-   * @param column int
-   * @return boolean
-   */
   @Override
   public boolean isCellEditable(int row, int column) {
     return (column == ETAT);
   }
 
-  /**
-   * setValueAt
-   *
-   * @param value  Object
-   * @param row    int
-   * @param column int
-   */
   @Override
   public void setValueAt(Object value, int row, int column) {
     values[row][column] = value;
   }
 
-  /**
-   * addString
-   *
-   * @param _sText  String
-   * @param _bState boolean
-   */
-  public void addString(String _sText, boolean _bState) {
 
+  void addString(String _sText, boolean _bState) {
     oVector.add(_sText);
     oBoolVector.add(_bState);
     values = new Object[oVector.size()][2];
     for (int i = 0; i < oVector.size(); i++) {
-      values[i][1] = oVector.get(i);
       values[i][0] = oBoolVector.get(i);
+      values[i][1] = oVector.get(i);
     }
   }
 
-  /**
-   * removeAll: Supprime toute les lignes
-   */
   public void removeAll() {
     values = new Object[1][2];
     values[0][0] = false;

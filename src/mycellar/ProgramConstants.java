a8975eff3e200dbc01c9893512f02316c8075f64
sebastien <sebastien4shopping@gmail.com>
2025-05-16 07:44:50 +0200

    Prepare version 80

--- src/mycellar/ProgramConstants.java
+++ src/mycellar/ProgramConstants.java
@@ -18,7 +18,7 @@ public final class ProgramConstants {
 
   public static final String INTERNAL_VERSION = "5.4.6.8";
   public static final int VERSION = 80;
-  public static final String MAIN_VERSION = VERSION + " BI";
+  public static final String MAIN_VERSION = VERSION + " VL";
   public static final String DEFAULT_STORAGE_EN = "Default storage";
   public static final String DEFAULT_STORAGE_FR = "Rangement par défaut";
 
